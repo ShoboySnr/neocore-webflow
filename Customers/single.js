6664257b18649d3789a484a51706d31197a62ec6
OLAMIDE <olaniyilamie@gmail.com>
2022-02-26 01:37:10 +0100

    created single.js file

--- Customers/single.js
+++ Customers/single.js
@@ -1,4 +1,4 @@
-<script>
+
   var myUrl = new URL(document.location.href)
   var userID = myUrl.searchParams.get("id")
 
@@ -117,6 +117,22 @@
       const customer_validations_work_address_el = document.getElementById('field-validations-work-address');
       customer_validations_work_address_el.textContent = readStates(customer_validations.WorkAddress);
 
+      // Edit Address
+      const edit_street1 = document.getElementById("street1")
+      edit_street1.value = customer.Street
+
+      const edit_street2 = document.getElementById("street2")
+      edit_street2.value = customer.Street2
+
+      const edit_town = document.getElementById("town")
+      edit_town.value = customer.Town
+
+      const edit_state = document.getElementById("state")
+      edit_state.value = customer.State
+
+      const edit_lga = document.getElementById("lga")
+      edit_lga.value = customer.Lga
+
       
       return;
     
@@ -136,6 +152,80 @@
   document.getElementById("button-customer-deactivate-customer").addEventListener("click", function() { 
       updateCustomerStatus(userID, false)
   });
+
+  //profile picture modal
+  document.getElementById("button-customer-update-picture").addEventListener("click", function() { 
+        e.preventDefault();
+        const profile_pictue = document.getElementById("profile_picture_id");
+        profile_pictue.style.display = "block";
+    });
+
+    document.getElementById("close_profile_modal").addEventListener("click", function(e) {
+      e.preventDefault();
+      const profile_pictue = document.getElementById("profile_picture_id");
+      profile_pictue.style.display = "none";
+    })
+
+    //Address modal
+    document.getElementById("button-customer-update-home-address").addEventListener("click", function(e) { 
+        e.preventDefault();
+        const user_address_container = document.getElementById("user_address_container_id");
+        user_address_container = "block";
+    });
+
+    document.getElementById("close_address_modal").addEventListener("click", function(e) {
+      e.preventDefault();
+      const user_address_container = document.getElementById("user_address_container_id");
+      user_address_container = "none";
+    })
+
+    document.getElementById("edit-customer-address-form").addEventListener("submit", (e)=>{
+      e.preventDefault();
+      e.stopPropagation();
+      const input_street1 = document.getElementById("street1").value
+
+      const input_street2 = document.getElementById("street2").value
+
+      const input_town = document.getElementById("town").value
+
+      const input_state = document.getElementById("state").value
+
+      const input_lga = document.getElementById("lga").value
+
+      let request = cbrRequest(`/users/${userID}/homeAddress`,'POST',true);
+
+      let data ={
+          "addressType": 1,
+          "street": input_street1,
+          "street2": input_street2,
+          "town": input_town,
+          "state": input_state,
+          "lga": input_lga
+        }
+
+      request.onload = function() {
+        
+        let data = JSON.parse(this.response);
+        // Status 200 = Success. Status 400 = Problem.  This says if it's successful and no problems, then execute
+        if (request.status >= 200 && request.status < 400) {
+        addNewProduct.reset();
+        const success_message = data.message;
+        
+        //show success message
+        let success_message_el = document.getElementById("success-message");
+        success_message_el.innerHTML = success_message;
+        success_message_el.style.display = "block";
+        
+        } else {
+                const failed_message = data.message;
+                let failed_message_el = document.getElementById("failed-message");
+            failed_message_el.innerHTML = failed_message;
+            failed_message_el.style.display = "block";
+        }
+      }
+      request.send(JSON.stringify(data));
+     
+    },true)
   
   function updateCustomerStatus(userID, activeStatus) {
 
@@ -174,4 +264,3 @@
       request.send();
   }
   
-  </script>
