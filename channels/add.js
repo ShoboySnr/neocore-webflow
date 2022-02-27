a8157bee4cf152fcd80f372580eb9eafdd050f7d
Damilare Shobowale <oluwadamilareshobowale@gmail.com>
2022-02-27 13:49:53 +0100

    added document

--- channels/add.js
+++ channels/add.js
@@ -361,6 +361,7 @@ function effectFieldTypeChange(slug, event) {
     const ranges_column = document.querySelectorAll('.ranges-' + slug + '-column');
 
     document.getElementById('field-channel-'+ slug + '-type').setAttribute('disabled', false);
+    console.log(document.querySelector('.button-' + slug +'-remove-extra-legs'));
     
     if(document.querySelector('.button-' + slug +'-remove-extra-legs').length > 1) {
         document.getElementById('field-channel-'+ slug + '-type').setAttribute('disabled', true);
