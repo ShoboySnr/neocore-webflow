0f38be6e4e87d7b34ca01c463626e6d0ee2e93f8
Damilare Shobowale <oluwadamilareshobowale@gmail.com>
2022-04-07 12:38:55 +0100

    fixes

--- LoanApplications/all.js
+++ LoanApplications/all.js
@@ -1,39 +1,30 @@
 let loanAppProducts;
 
 async function getLoanProducts() {
-    fbauth.onAuthStateChanged((user) => {
-        
-        if(user) {
-            fbauth.currentUser.getIdToken(true).then(async (idToken) => {
-                let request = await cbrRequest('/loanAppProducts', 'GET', true, idToken);
+    let request = await cbrRequest('/loanAppProducts', 'GET', true, idToken);
                 
-                request.onload = function() {
-        
-                    if (request.status >= 200 && request.status < 400) {
-                          let data = JSON.parse(this.response);
+    request.onload = function() {
 
-                          console.log(data);
-                        
-                          loanAppProducts = data.data;
-                          
-                          let parent_el = document.getElementById("field-loan-applications-products");
-                          appendToSelect(loanAppProducts, parent_el);
+        if (request.status >= 200 && request.status < 400) {
+              let data = JSON.parse(this.response);
 
-                          document.getElementById('field-loan-applications-products').addEventListener('change', (event) => {
-                            let target = event.target;
-                            
-                            //add to the stages
-                            document.getElementById('field-loan-applications-stage').innerHTML = '';
+              console.log(data);
+            
+              loanAppProducts = data.data;
+              
+              let parent_el = document.getElementById("field-loan-applications-products");
+              appendToSelect(loanAppProducts, parent_el);
 
-                          });
-                    }
-                  }
-                  request.send();
+              document.getElementById('field-loan-applications-products').addEventListener('change', (event) => {
+                let target = event.target;
+                
+                //add to the stages
+                document.getElementById('field-loan-applications-stage').innerHTML = '';
 
-
-            });
+              });
         }
-    });
+      }
+      request.send();
 }
 
 function populateStage(product_stages) {
