687969e7d9b17ee0ed023bba714f6c29d9243c38
Daniel Olanrewaju <danorelanre@gmail.com>
2022-04-07 14:46:53 +0100

    attempted loan account get filter

--- LoanAccounts/all.js
+++ LoanAccounts/all.js
@@ -1,5 +1,61 @@
 async function getLoanAccounts() {
-    let request = await cbrRequest('/loanAccounts?name=&amtFrom=&amtTp=&product=&status&disbFrom=&disbTo=', 'GET', true)
+    let request = await cbrRequest('/loanAccounts', 'GET', true)
+    //?name=&amtFrom=&amtTp=&product=&status&disbFrom=&disbTo=
+
+    request.onload = function () {
+        let data = JSON.parse(this.response)
+        if (request.status >= 200 && request.status < 400) {
+            const cardContainer = document.getElementById("customers-container")
+            let counter = 0;
+            data.data.forEach(account => {
+                const style = document.getElementById('sample-customer')
+                const card = style.cloneNode(true)
+
+                card.setAttribute('id', '');
+                card.style.display = 'block';
+
+                card.addEventListener('click', function () {
+                    document.location.href = "/loan-accounts/ingle-loan-account?id=" + customer.ID;
+                });
+
+                const customer_name_el = card.getElementsByTagName('p')[0]
+                customer_name_el.textContent = account.customer_name;
+
+                const product_id_el = card.getElementsByTagName('p')[1]
+                product_id_el.textContent = account.product_id;
+
+                const amount_el = card.getElementsByTagName('p')[2]
+                amount_el.textContent = format_currency(account.amount);
+
+                const outstanding_el = card.getElementsByTagName('p')[3]
+                outstanding_el.textContent = format_currency(account.outstanding);
+
+                const arrears_el = card.getElementsByTagName('p')[4]
+                arrears_el.textContent = format_currency(account.arrears);
+
+                const status_el = card.getElementsByTagName('p')[4]
+                status_el.textContent = account.status;
+
+                cardContainer.appendChild(card);
+            })
+        }
+    }
+
+    request.send();
+}
+
+async function filterLoanAccounts(e)
+{
+    e.preventDefault();
+    let formData = new FormData(this);
+    let name = formData.get("field-customer-name");
+    let nuban = formData.get("field-nuban");
+    let product_name =  returnSelected(document.getElementById('field-product-name'))
+    let status = returnSelected(document.getElementById('field-status'));
+    let request = await cbrRequest('/loanAccounts?name='
+        + name +
+        '&amtFrom=&amtTp=&product='
+        + product_name + '&status=' + status + '&disbFrom=&disbTo=', 'GET', true)
 
     request.onload = function () {
         let data = JSON.parse(this.response)
@@ -44,6 +100,10 @@ async function getLoanAccounts() {
 }
 
 
+//wf-form-Get-GL-Form
+
+const addNewProduct = document.getElementById("wf-form-Get-GL-Form")
+addNewProduct.addEventListener('submit', filterLoanAccounts);
 
 window.addEventListener('firebaseIsReady', () => {
    getLoanAccounts();
