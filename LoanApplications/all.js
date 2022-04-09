f60ab5d1ac1f2914ef8565972ee33230a654798f
Daniel Olanrewaju <danorelanre@gmail.com>
2022-04-09 13:44:57 +0100

    get loan application list

--- LoanApplications/all.js
+++ LoanApplications/all.js
@@ -1,7 +1,7 @@
 let loanAppProducts;
 
 async function getLoanProducts() {
-    let request = await cbrRequest('/loanForms/stages', 'GET', true);
+    let request = await cbrRequest('/loanAppProducts', 'GET', true);
                 
     request.onload = function() {
 
@@ -9,7 +9,6 @@ async function getLoanProducts() {
               let data = JSON.parse(this.response);
 
               console.log(data);
-              return;
             
               loanAppProducts = data.data.products;
               
