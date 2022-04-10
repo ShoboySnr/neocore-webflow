f4e04d5f7be14595878bbad3bc932e31d1a0dc8e
Damilare Shobowale <oluwadamilareshobowale@gmail.com>
2022-04-10 08:34:03 +0100

    troubleshoot escalate button

--- LoanApplications/single.js
+++ LoanApplications/single.js
@@ -10,9 +10,7 @@
 let myUrl = new URL(document.location.href)
 let applicationID = myUrl.searchParams.get("id")
 
-async function escalateFormAction (event) {
-    event.preventDefault();
-
+async function escalateFormAction () {
     let target = document.getElementById('wf-form-EscalateForm');
 
     let formData = new FormData(target);
