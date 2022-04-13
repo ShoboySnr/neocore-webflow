6f21ff8d02685ebfb61fb22f78e3a12e4c2d8bbd
Daniel Olanrewaju <danorelanre@gmail.com>
2022-04-13 12:15:00 +0100

    get loan application single

--- LoanApplications/single.js
+++ LoanApplications/single.js
@@ -539,7 +539,8 @@ function toggleUploadedFiles(e)
     let expandedList = element.children[1];
     console.log(e.target.classList);
     console.log(collapsedList.classList);
-    console.log(expandedList.classList);
+    console.log(expandedList.classList.value);
+    console.log(expandedList.classList.values());
     console.log([collapsedList, expandedList]);
     return;
     if (e.target.classList.includes("expanded-list")) return;
